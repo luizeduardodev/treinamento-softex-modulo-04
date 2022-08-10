528e3bf5fc7d970e4e1fbd706cf1784558fee3cf
Luiz Eduardo <luizeduardorodrigues807@gmail.com>
2022-08-10 11:44:23 -0300

    exercício adicionado

---
+++ manipulacaoString.js
@@ -0,0 +1,15 @@
+// Estruture três códigos, os quais devem conter uma função de manipulação de string que obtenha resultado.
+
+// Trabalhe esse código em seu IDE, suba ele para sua conta no GitHub e compartilhe o link desse projeto no campo ao lado para que outros desenvolvedores possam analisá-lo.
+
+const nome = "Luiz ";
+const sobrenome = "Eduardo";
+
+const concatenar = nome.concat(sobrenome);
+console.log(concatenar);
+
+const toUpperCase = nome.toUpperCase();
+console.log(toUpperCase);
+
+const indiceCaractere = nome.charAt(0);
+console.log(indiceCaractere);
